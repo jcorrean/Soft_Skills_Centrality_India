8cbd0c06fed1e1185b973a5f6ba03db7cef85622
Juan C. Correa <j.correa.n@gmail.com>
2023-11-29 14:23:44 -0600

    Update ColoringEdges.R

--- ColoringEdges.R
+++ ColoringEdges.R
@@ -1,21 +1,19 @@
+library(igraph)
+library(readr)
+# Your data
 data <- read_csv("HypotheticalData.csv")
 
 # Create a bipartite graph
 bn <- graph_from_data_frame(data, directed = FALSE)
-V(bn)$type <- mapping$type
-V(bn)$color <- ifelse(V(bn)$type, "red", "darkgreen")
 
+bipartite.mapping(bn)
+V(bn)$type <- bipartite_mapping(bn)$type
+V(bn)$color <- ifelse(V(bn)$type, "#FF671F", "#046A38")
 # Set edge attributes
-E(bn)$color <- ifelse(E(bn)$ProgramType == "Undergraduate", "red", "green")
+E(bn)$linetype <- ifelse(E(bn)$ProgramType == "Undergraduate", 1, 2)  # 1 for solid, 2 for dashed
 
-# Plot the bipartite network with colored edges
-plot(
-  bn,
-  vertex.label.color = "black",
-  vertex.label.cex = 0.8,
-  vertex.size = 15,
-  edge.width = 1,
-  edge.color = E(bn)$color,
-  layout = layout_nicely,
-  main = "Bipartite Network"
-)
+
+layout <- layout_as_bipartite(bn)
+rotated_layout <- cbind(layout[, 2], -layout[, 1])  # Swap x and y coordinates and negate y
+plot(bn, vertex.label = V(bn)$name, layout = rotated_layout, main = "",
+     vertex.label.color = "black", edge.lty = E(bn)$linetype)
