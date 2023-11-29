4f2faa5bbef0c3ff7b77f4594faf17154a004d75
Juan C. Correa <j.correa.n@gmail.com>
2023-11-28 23:46:01 -0600

    Create ColoringEdges.R

---
+++ ColoringEdges.R
@@ -0,0 +1,31 @@
+library(readr)
+library(igraph)
+
+# Your data
+data <- read_csv("HypotheticalData.csv")
+
+# Create a bipartite graph
+bn <- graph_from_data_frame(data, directed = FALSE)
+
+# Get bipartite mapping
+mapping <- bipartite_mapping(bn)
+V(bn)$type <- mapping$type
+
+# Set vertex attributes
+V(bn)$color <- ifelse(V(bn)$type, "red", "green")
+V(bn)$shape <- ifelse(V(bn)$type, "circle", "square")
+V(bn)$label.cex <- ifelse(V(bn)$type, 0.5, 1)
+V(bn)$size <- sqrt(degree(bn))
+E(bn)$color <- 'green'
+
+
+# Plot the bipartite network
+plot(
+  bn,
+  vertex.label.color = "black",
+  vertex.label.cex = 0.8,
+  vertex.size = 15,
+  edge.width = 1,
+  layout = layout_as_bipartite,
+  main = "Bipartite Network"
+)
