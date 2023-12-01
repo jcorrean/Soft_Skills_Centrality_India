878aecfac0212437d7d1b5ebbb899891814a8372
Juan C. Correa <j.correa.n@gmail.com>
2023-12-01 01:16:35 -0600

    Update Step4.R

--- Step4.R
+++ Step4.R
@@ -16,7 +16,7 @@ Programs <- Programs[ -c(5:25) ]
 rownames(Programs)
 Programs$SS <- rownames(Programs)
 Programs <- Programs[order(Programs$SS), ]
-Programs <- Programs[490:535,]
+Programs <- Programs[grepl("S", Programs$SS), ]
 Programs <- Programs[1:4]
 colnames(Programs)[4] <- "Eigenvector"
 
@@ -39,9 +39,11 @@ dev.off()
 
 IM <- as_incidence_matrix(BNA, names = TRUE, sparse = TRUE, types = bipartite_mapping(BNA)$type)
 IM2 <- as.matrix(IM)
+rownames(Programs)[order(Programs$Eigenvector, decreasing = TRUE)]
 # Let's pick the most important soft skills
 # as per their eigenvector centrality
-selected_columns <- c("S10", "S42", "S44", "S20", "S18", "S3", "S4", "S12", "S6", "S34")
+selected_columns <- c("S10", "S42", "S18", "S44", "S12", "S3",  "S20", "S4",  "S6", "S34")
+                      
 
 # Subset the matrix by column names
 IM3 <- IM2[, selected_columns, drop = FALSE]
