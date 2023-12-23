014f7f0752a477e37a7f4c691dccb7f239c64c4e
Juan C. Correa <j.correa.n@gmail.com>
2023-12-23 09:41:21 -0600

    Update Fig1.R

--- Fig1.R
+++ Fig1.R
@@ -11,11 +11,11 @@ V(bn)$type <- bipartite_mapping(bn)$type
 V(bn)$color <- ifelse(V(bn)$type, "#FF671F", "#046A38")
 V(bn)$shape == "none" 
 V(bn)$labelcolor <- ifelse(V(bn)$type, "#FF671F", "#046A38")
-E(bn)$color <- "lightblue"
+E(bn)$color <- "#06038D"
 E(bn)$width <- 1.9
 
 # Set edge attributes
-E(bn)$linetype <- ifelse(E(bn)$ProgramType == "Undergraduate", 1, 2)  # 1 for solid, 2 for dashed
+E(bn)$linetype <- ifelse(E(bn)$ProgramType == "Bachelor", 1, 2)  # 1 for solid, 2 for dashed
 
 
 layout <- layout_as_bipartite(bn)
