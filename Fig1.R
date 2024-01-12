6851a76cabfcc04be472f71ce2a4bb668ce0368e
Juan C. Correa <j.correa.n@gmail.com>
2024-01-12 04:24:44 -0600

    Update Fig1.R

--- Fig1.R
+++ Fig1.R
@@ -12,7 +12,7 @@ V(bn)$color <- ifelse(V(bn)$type, "#FF671F", "#046A38")
 V(bn)$shape == "none" 
 V(bn)$labelcolor <- ifelse(V(bn)$type, "#FF671F", "#046A38")
 E(bn)$color <- "#06038D"
-E(bn)$width <- 1.9
+E(bn)$width <- 1.5
 
 # Set edge attributes
 E(bn)$linetype <- ifelse(E(bn)$ProgramType == "Bachelor", 1, 2)  # 1 for solid, 2 for dashed
