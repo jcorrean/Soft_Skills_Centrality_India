0dc997d422cd5eea7dc3073434c1dfdc2ac3c2ff
Juan C. Correa <j.correa.n@gmail.com>
2024-01-13 13:31:17 -0600

    Update Step7.R

--- Step7.R
+++ Step7.R
@@ -1,1 +1,14 @@
 load("Results/Result4.RData")
+
+library(bipartite)
+png("F3.png", width = 20, height = 7, units = 'in', res = 300)
+plotweb(IM3, method = "normal", 
+        col.high = "orange", 
+        bor.col.high = "orange",
+        col.low = "darkgreen", 
+        bor.col.low = "darkgreen",
+        col.interaction = "grey90",
+        bor.col.interaction = "grey90",
+        low.lablength = 0,
+        labsize = 2)
+dev.off()
