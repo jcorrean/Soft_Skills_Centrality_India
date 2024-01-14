730470bc316e2cb75b91296b241955c0315ba319
Juan C. Correa <j.correa.n@gmail.com>
2024-01-13 19:39:29 -0600

    Update Step7.R

--- Step7.R
+++ Step7.R
@@ -1,4 +1,21 @@
-load("Results/Result4.RData")
+load("Results/Result6.RData")
+rm(list=setdiff(ls(), c("Bachelors", "Masters", "Doctorates")))
+
+bachelors <- graph.data.frame(Bachelors, directed = FALSE)
+BachelorNetwork <- data.frame(Degree = igraph::degree(bachelors),
+                       Closeness = igraph::closeness(bachelors),
+                       Betweennes = igraph::betweenness(bachelors),
+                       Eigen = igraph::eigen_centrality(bachelors))
+BachelorNetwork <- BachelorNetwork[ -c(5:25) ]
+rownames(BachelorNetwork)
+BachelorNetwork$SS <- rownames(BachelorNetwork)
+BachelorNetwork <- BachelorNetwork[order(BachelorNetwork$SS), ]
+BachelorNetwork <- BachelorNetwork[grepl("S", BachelorNetwork$SS), ]
+BachelorNetwork <- BachelorNetwork[1:4]
+colnames(BachelorNetwork)[4] <- "Eigenvector"
+TopBachelorSkills <- head(BachelorNetwork[order(-BachelorNetwork$Closeness), ], 10)
+
+
 
 library(bipartite)
 png("F3.png", width = 20, height = 7, units = 'in', res = 300)
