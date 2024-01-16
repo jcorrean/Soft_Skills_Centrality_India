6b1a6a642752593db6abf6dd9cc59c313064b846
Juan C. Correa <j.correa.n@gmail.com>
2024-01-15 21:03:13 -0600

    Update Step8.R

--- Step8.R
+++ Step8.R
@@ -1,2 +1,6 @@
 load("Results/Result7.RData")
 rm(list=setdiff(ls(), c("BachelorNetwork", "MastersNetwork", "PHDNetwork")))
+
+BachelorNetwork$Level <- "Bachelor Program"
+MastersNetwork$Level <- "Master Program"
+PHDNetwork$Level <- "PhD Program"
