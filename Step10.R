dc24c75dbcc0d6d8a4bbfca2bacd053fb61ac994
Juan C. Correa <j.correa.n@gmail.com>
2025-10-02 14:33:28 -0600

    Update Step10.R

--- Step10.R
+++ Step10.R
@@ -4,10 +4,26 @@ library(ergm)
 library(network)
 library(coda)
 set.seed(9173)
-Model1 <- ergm(red ~ edges + b1sociality(c(3, 11, 13)), 
+network::network.vertex.names(India)
+network::network.vertex.names(India)
+#[1] "Communication"       "Ethical Thinking"    "Strategic Thinking" 
+#[4] "Teamwork"            "Entrepreneurship"    "Analytical Thinking"
+#[7] "Critical Thinking"   "Leadership"          "Decision Making"    
+#[10] "Problem Solving"     "Creativity"          "Negotiation"        
+#[13] "Self-Awareness" 
+Model1 <- ergm(India ~ edges + b1sociality(1:13), 
                control = control.ergm(MCMC.samplesize = 10000,
                                       MCMC.burnin = 5000,
                                       MCMLE.maxit = 10))
-summary(ModelB) # AIC = 7664
-GOF <- gof(ModelB)
+summary(Model1) # AIC = 7664
+GOF <- gof(Model1)
+plot(GOF)
+
+Model1A <- ergm(India ~ edges + b1sociality(c(1, 8)) +
+                  b2factor(), 
+               control = control.ergm(MCMC.samplesize = 10000,
+                                      MCMC.burnin = 5000,
+                                      MCMLE.maxit = 10))
+summary(Model1) # AIC = 7664
+GOF <- gof(Model1)
 plot(GOF)
